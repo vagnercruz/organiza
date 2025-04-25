86b614eb0ef8d91046ec0b27982110efc4a7fb54
vagnercruz <vagnerdacruz48@gmail.com>
2025-04-25 13:58:16 -0300

    Adicionando a tela de dashboard

---
+++ src/view/DashboardView.java
@@ -0,0 +1,38 @@
+package view;
+
+import model.Usuario;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DashboardView extends JFrame {
+    private Usuario usuario;
+    public DashboardView(Usuario usuario){
+        this.usuario = usuario;
+
+        setTitle("Painel Principal");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        JLabel bemVindo = new JLabel("Bem-vindo, " + usuario.getNome() + "!");
+        bemVindo.setHorizontalAlignment(SwingConstants.CENTER);
+
+        JButton btnNovaTransacao = new JButton("Nova Transação");
+        JButton btnVerTransacoes = new JButton("Ver Transações");
+        JButton btnSaldo = new JButton("Consultar Saldo");
+        JButton btnLogout = new JButton("Logout");
+
+        JPanel painel = new JPanel();
+        painel.setLayout(new GridLayout(5,1,10,10));
+        painel.add(bemVindo);
+        painel.add(btnNovaTransacao);
+        painel.add(btnVerTransacoes);
+        painel.add(btnSaldo);
+        painel.add(btnLogout);
+
+        add(painel);
+        setVisible(true);
+
+    }
+}
